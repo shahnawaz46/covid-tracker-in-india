94560932a5e1499796b63dcb23e60eeaf5133705
shahnawaz <shahnawazmd791@gmail.com>
2021-07-30 18:01:19 +0530

    Create a React app and publish it to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CovidCases1 from "./components/CovidCases1";
 import CovidCases2 from "./components/CovidCases2";
 
 
 
 function App() {
+  const [singleData, setSingleData] = useState({});
+  const [stateData, setStateData] = useState([]);
+
+  const getApiData = async () => {
+    try {
+      const response = await fetch('https://api.covid19india.org/data.json')
+      const data = await response.json()
+
+      setSingleData(data.statewise[0])
+
+      const state = data.statewise.slice(1)
+      setStateData(state)
+
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  useEffect(() => {
+    getApiData()
+  }, [])
+
 
   return (
     <>
-      <CovidCases1 />
-      <CovidCases2 />
+      <CovidCases1 covidData={singleData} />
+      <CovidCases2 covidData={stateData} />
 
     </>
   );
--- src/components/CovidCases1.js
+++ src/components/CovidCases1.js
@@ -2,36 +2,19 @@ import React, { useEffect, useState } from 'react';
 import CovidCases1Card from './CovidCases1Card';
 import './CovidCases1.css';
 
-const CovidCases1 = () => {
-    const [oneData, setOneData] = useState({});
-
-    const getCovidDataApi = async() =>{
-        try{
-            const response = await fetch('https://api.covid19india.org/data.json')
-            const data = await response.json()
-            setOneData(data.statewise[0])
-            console.log(data.statewise[0])
-
-        }catch(error){
-            alert("Error: Please come after few time")
-        }
-    }
-
-    useEffect(()=>{
-        getCovidDataApi()
-    }, [])
-
+const CovidCases1 = ({covidData}) => {
 
     return (
         <div className="main-div">
             <h2>Covid Cases In INDIA</h2>
+            <span className="live">🔴 LIVE</span>
             <div className="cards">
                 <CovidCases1Card value1="Country" value2="INDIA" color="blue" />
-                <CovidCases1Card value1="Total Case" value2={oneData.confirmed} color="Grey" />
-                <CovidCases1Card value1="Recoverd" value2={oneData.recovered} color="green" />
-                <CovidCases1Card value1="Deaths" value2={oneData.deaths} color="red" />
-                <CovidCases1Card value1="Active Case" value2={oneData.active} color="Grey" />
-                <CovidCases1Card value1="Last Update" value2={oneData.lastupdatedtime} color="black" />
+                <CovidCases1Card value1="Total Case" value2={covidData.confirmed} color="Grey" />
+                <CovidCases1Card value1="Recoverd" value2={covidData.recovered} color="green" />
+                <CovidCases1Card value1="Deaths" value2={covidData.deaths} color="red" />
+                <CovidCases1Card value1="Active Case" value2={covidData.active} color="Grey" />
+                <CovidCases1Card value1="Last Update" value2={covidData.lastupdatedtime} color="black" />
             </div>
         </div>
     );
